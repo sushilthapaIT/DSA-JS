32c0f419cb1d856d3cc27ef0df26ad0503da0a4a
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-25 23:35:22 -0400

    DSA- stack
    // Stacks are last in first out type of service.

---
+++ stack.js
@@ -0,0 +1,3 @@
+/* STACK */
+// Stacks are last in first out type of service.
+// Meaning: Lets take an example of a book the last book you put on top of a stack would be the first book you take off the stack.
