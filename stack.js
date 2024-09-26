89743e950e6f644d0c6e56af9b78ba157223ace4
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-25 23:59:17 -0400

    code add
    -- code added
    -- example used as a palindrome word to learn about stack

--- stack.js
+++ stack.js
@@ -1,3 +1,31 @@
 /* STACK */
-// Stacks are last in first out type of service.
-// Meaning: Lets take an example of a book the last book you put on top of a stack would be the first book you take off the stack.
+
+/*
+Stacks are last in first out type of service.
+Meaning: Lets take an example of a book the last book you put on top of a stack would be the first book you take off the stack.
+functions:  push = for placing data onto a stack
+            pop = for removing the top element of a stack
+            peek = for displaying the top element of the stack
+            length or size = for determining how many elements are on a stack
+*/
+
+var letters = [];
+var word = "racecar";
+var reversedWord = "";
+
+// put letters of word into stack
+for (var i = 0; i < word.length; i++){
+    letters.push(word[i]);
+}
+
+// pop off the stack in reverse order
+for (var i = 0; i < word.length; i++){
+    reversedWord += letters.pop();
+}
+
+if(reversedWord === word){
+    console.log(word + " is a palindrome.")
+}
+else{
+    console.log(word + " is not a palindrome.")
+}
