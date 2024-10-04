ebff384d4dc409555cb0f0f4db2a291ce3eae020
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-04 16:21:52 -0400

    update

--- fin2ndLarge.js
+++ fin2ndLarge.js
@@ -2,12 +2,43 @@
 To find second largest value, and remove first largest value in a array
 */
 
-const arrNum = [1,2,3,4,5,66];
+// const arrNum = [1,2,3,4,5,66];
 
-const maxNum = (arr) => {
-    return arr.reduce(function(preValue, currentValue){
-        return preValue > currentValue ? preValue:currentValue;
-    })
+// const maxNum = (arr) => {
+//     return arr.reduce(function(preValue, currentValue){
+//         return preValue > currentValue ? preValue:currentValue;
+//     })
+// }
+
+// console.log(maxNum(arrNum))
+
+/* 
+another method to find largest and minimum value in array 
+*/
+
+
+/*
+const largestValue = (arr) => {
+    return firstLargeValue=Math.max(...arr);
 }
 
-console.log(maxNum(arrNum))
+console.log(largestValue(arrNum));
+*/
+
+/* another method to find largest and minimum value in array */
+/*
+const minValue = (arr) => {
+    return firstMinValue=Math.min(...arr);
+}
+
+console.log(minValue(arrNum));
+*/
+
+const intArray = [1,2,3,4,5];
+
+const largeValue = (arr) => {
+    firstLargestValue=Math.max(...arr);
+    return firstLargestValue;
+}
+
+console.log(largeValue(intArray))
