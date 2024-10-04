a16f0ef874da33aacd1e963a64013c43ded36f43
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-04 17:58:31 -0400

    added array of object

--- filter&find.js
+++ filter&find.js
@@ -4,4 +4,22 @@ Difference between filter and find method
 
 /*
 filter() => this method returns the matched values in an array from the collection
+find()   => this method returns the first value that match from the collection once it matched the value in findings,
+            it will not check the remaining values in the array collection.   
 */
+
+//example 
+
+const exampleArray = [
+    {name: "king", age: 21},
+    {name: "queen", age: 22},
+    {name: "ace", age: 44},
+    {name: "roha", age: 31},
+    {name: "sam", age: 34},
+    {name: "george", age: 23},
+    {name: "cali", age: 22},
+    {name: "tessa", age: 24},
+    {name: "kat", age: 28},
+    {name: "rhitu", age: 24},
+    {name: "raha", age: 19},
+];
