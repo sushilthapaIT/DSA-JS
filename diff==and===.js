2851415baa680cb7cde97156d3ae86bd0a534b96
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-03 23:57:27 -0400

    notes added

--- diff==and===.js
+++ diff==and===.js
@@ -1,3 +1,10 @@
 /*
 Difference between ==  and ===
 */
+
+/*
+Both are comparison operator.
+The main difference between the both operators is that,
+"==" is used to compares values
+"===" is used to compare both value and types. 
+*/
