6e7a2a8d999a58ed50f9cda8d5309cae1797fd43
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-04 00:00:35 -0400

    example added for "==="

--- diff==and===.js
+++ diff==and===.js
@@ -16,4 +16,13 @@ Examples:
 var x = 7;
 var y = '7';
 
-console.log(x==y);
+console.log(x==y); // the result will be true it compares the value but ignore its data types.
+
+/*
+"==="
+Examples:
+*/
+var x = 7;
+var y = '7';
+
+console.log(x==y); // the result will be false it compares the value and its data types since one is integer and another one is string it will return false.
