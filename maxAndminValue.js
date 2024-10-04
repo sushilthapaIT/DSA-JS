1d96644dd97ea3fce295252499ee1f731cc011e3
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-03 23:35:12 -0400

    done with finding the current max number in a array

--- maxAndminValue.js
+++ maxAndminValue.js
@@ -1,3 +1,13 @@
 /*
 To find the max and min value in a given array
 */
+
+const arrNumber = [1,2,3,4,5,6,7,8,9,10];
+
+const maxFunction = (arr) => {
+    return arr.reduce(function(preValue, currValue){
+        return preValue > currValue ? preValue:currValue;
+    })
+}
+
+console.log(maxFunction(arrNumber))
