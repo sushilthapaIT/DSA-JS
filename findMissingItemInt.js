f85c9f5f57def0391d5b8c9c829a52ac125ebe73
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-04 18:56:13 -0400

    To find the missing number in a given integer from array

--- findMissingItemInt.js
+++ findMissingItemInt.js
@@ -1,3 +1,19 @@
 /*
 To find the missing number in a given integer from array
 */
+
+const arrNum = [1,2,3,5,6,7,8,100];
+const missingNum = [];
+
+const missingValue = (arr) => {
+    const minValue = Math.min(...arr);
+    const maxValue = Math.max(...arr);
+    for(let i = minValue; i < maxValue; i++){
+        if(arr.indexOf(i) < 0){
+            missingNum.push(i);
+        }
+    }
+    return {missingNum};
+};
+
+console.log(missingValue(arrNum));
